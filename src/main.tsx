fc7219e5219e01e898c0646866a48c44fbef5a04
nobulldev <systems@nobull-strategies.com>
2026-08-12 08:57:38 -0300

    feat: add Google Analytics tracking tag to index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import { initGA } from "./lib/googleAnalytics.ts";
 import App from "./App.tsx";
 import "./index.css";
 
-initGA();
+//initGA();
 
 createRoot(document.getElementById("root")!).render(<App />);
